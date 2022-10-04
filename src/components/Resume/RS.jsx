95a960c8584f95083d43c365f3018386bb4efac4
altinawiwaseem <wasemm@live.com>
2022-10-04 22:10:32 +0200

    adding selected class and scroll into fuction

--- src/components/Resume/RS.jsx
+++ src/components/Resume/RS.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./RS.css";
 import { resumeData } from "./data";
+
 function RS() {
+  const targetedBox = useRef(null);
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    targetedBox.current.children[selected]?.scrollIntoView({
+      behavior: "smooth",
+    });
+    return;
+  }, [selected]);
+
   return (
     <div className="r-main-container">
       <div className="r-header"></div>
@@ -10,21 +21,28 @@ function RS() {
       <span className="underline">
         <span className="underline-node"></span>
       </span>
+      {/* description container */}
       <div className="r-content-container">
         <div className="r-left">
           <div className="bullet-icons"></div>
+
           {resumeData.map((item, i) => (
-            <div key={i} id={i} className="r-bullet-box">
+            <div
+              key={i}
+              id={i}
+              onClick={() => setSelected(i)}
+              className={`r-bullet-box ${i == selected ? "selected" : ""}`}
+            >
               <span className="logo">{item.icon}</span>
-              <h2 className="bullet-label">{item.header}</h2> <span></span>
+              <h2 className="bullet-label">{item.header}</h2>
             </div>
           ))}
         </div>
-        <div className="r-right">
+        <div className="r-right" ref={targetedBox}>
           {resumeData.map((item, i) => (
             <div key={i} id={i} className={"r-details-container"}>
-              {item.title.map((el) => (
-                <div className="resume-element-detail">
+              {item.title.map((el, i) => (
+                <div key={i} className="resume-element-detail">
                   <div className="resume-element-header">
                     <span className="r-circle"></span>
                     <h3>{el.headerDescription}</h3>
