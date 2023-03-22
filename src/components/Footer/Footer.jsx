48cc10c347ec925488aca02a8e55218e9b863651
altinawiwaseem <wasemm@live.com>
2023-03-22 22:26:20 +0100

    add the footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { FaGithubSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Footer = () => {
-  return <div className="f">Footer</div>;
+  return (
+    <footer className="footer bg-primary-color text-white py-12 flex flex-col items-center mt-12 sm:font-poppins-semibold font-poppins-regular sm:text-sm md:text-lg">
+      <ul className=" flex flex-wrap justify-center gap-12 mx-auto mb-12">
+        <li>
+          {" "}
+          <Link to="home" spy={true} smooth={true} offset={-20} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" spy={true} smooth={true} offset={-15} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="resume"
+            spy={true}
+            smooth={true}
+            offset={-15}
+            duration={500}
+          >
+            Resume
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-15}
+            duration={500}
+          >
+            {" "}
+            Contact Me
+          </Link>
+        </li>
+      </ul>
+      <div className="flex justify-center gap-12 mb-16 text-2xl md:text-3xl text-white">
+        <a
+          className="icon"
+          href="https://github.com/altinawiwaseem"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare />
+        </a>
+        <a
+          className="icon"
+          href="https://www.linkedin.com/in/waseem-altinawi/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          className="icon"
+          href="https://www.instagram.com/waseemdam/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+      <div>
+        <small>&copy; WASEEM ALTINAWI. All rights reserved. </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,10 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 100) {
+      if (
+        window.scrollY > 100 &&
+        window.innerHeight + window.scrollY + 210 < document.body.scrollHeight
+      ) {
         handleScrolling(true);
       } else if (window.scrollY < 100) {
         handleScrolling(false);
@@ -39,58 +42,7 @@ const Navbar = () => {
 
   return (
     <>
-      {isMobile || scrolling ? (
-        <div className="floating-navbar bg-primary-color bg-opacity-30 backdrop-blur-[15px] max-w-max fixed bottom-5 flex rounded-full z-50">
-          <ul className="active py-3 px-7 flex gap-4 ">
-            <li>
-              {" "}
-              <Link
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-20}
-                duration={500}
-              >
-                {<AiOutlineHome />}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-15}
-                duration={500}
-              >
-                {<AiOutlineUser />}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="resume"
-                spy={true}
-                smooth={true}
-                offset={-15}
-                duration={500}
-              >
-                {<ImProfile />}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-15}
-                duration={500}
-              >
-                {" "}
-                {<BiMessageSquareDetail />}
-              </Link>
-            </li>
-          </ul>
-        </div>
-      ) : (
+      {isMobile || (
         <div className=" nav w-full sm:w-[60%] md:w-[50%]  flex justify-between sm:mt-10 mt-2">
           <div className="sm:w-1/7  "></div>
           <ul className="flex w-full justify-center tablet:justify-around  sm:font-poppins-semibold font-poppins-regular sm:text-sm md:text-lg">
@@ -138,6 +90,60 @@ const Navbar = () => {
                 duration={500}
               >
                 Contact Me
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      {((isMobile &&
+        window.innerHeight + window.scrollY + 210 <
+          document.body.scrollHeight) ||
+        scrolling) && (
+        <div className="floating-navbar bg-primary-color bg-opacity-30 backdrop-blur-[15px] max-w-max fixed bottom-5 flex rounded-full z-50">
+          <ul className="active py-3 px-7 flex gap-4 ">
+            <li>
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-20}
+                duration={500}
+              >
+                {<AiOutlineHome />}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-15}
+                duration={500}
+              >
+                {<AiOutlineUser />}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="resume"
+                spy={true}
+                smooth={true}
+                offset={-15}
+                duration={500}
+              >
+                {<ImProfile />}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-15}
+                duration={500}
+              >
+                {" "}
+                {<BiMessageSquareDetail />}
               </Link>
             </li>
           </ul>
